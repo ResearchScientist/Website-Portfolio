800ec421693933bf256d1b5ac87e7c345d962fb6
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2026-02-24 19:47:12 -0800

    [fix] firefox layout shift : add empty script to body to help dom populate before parsing styles

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ window.onfocus = function() {
 window.onblur = function() {
   document.title = "RS Portfolio : Come Back";
 }
+
+const portfolioButton = document.getElementById('web-sub-name');
+
+portfolioButton.addEventListener('click',transformName);
+
+function transformName() {
+  console.log('clicked portfolio');
+  
+}
