b2d85c08139f63bcd6c3d4001a3d345f27fb83a8
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2026-03-03 20:50:16 -0800

    [ux] demo reel : add fb seconds indicator

--- js/art.js
+++ js/art.js
@@ -4,6 +4,7 @@ const demoReelBody = document.getElementById('demo-reel-body');
 const videoDemoReel = document.getElementById('video-demo-reel');
 const videoKeys = document.getElementById('video-keys');
 const arrowsForward = document.getElementById('arrows-forward');
+const arrowsBackward = document.getElementById('arrows-backward');
 let cursorTimer;
 
 const darken = () => {
@@ -61,11 +62,15 @@ if (videoDemoReel) {
         arrowsForward.style.opacity = 1;
         setTimeout(() => {
           arrowsForward.style.opacity = 0;
-        },500);
+        },400);
         break;
       case 'arrowleft':
       case 'j':
         videoDemoReel.currentTime -= 10;
+        arrowsBackward.style.opacity = 1;
+        setTimeout(() => {
+          arrowsBackward.style.opacity = 0;
+        },400);
         break;
     }
   });
