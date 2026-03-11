ac1f2c3750674d464857db6f277ed7305fd7f363
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2026-03-10 20:16:02 -0700

    [ui] main nav : update submenu logic

--- js/main.js
+++ js/main.js
@@ -8,13 +8,23 @@ window.onblur = function() {
   document.title = "RS Portfolio : Come Back";
 }
 
+// MAIN NAV SUB MENU
+
 const projectsMenuButton = document.getElementById('projects-menu-button');
 const projectsSubMenu = document.getElementById('projects-sub-menu');
 
-projectsMenuButton.addEventListener('click',toggleSubMenu);
+document.addEventListener('click',toggleSubMenu);
 
-function toggleSubMenu() {
-  projectsSubMenu.classList.toggle('show-sub-menu');
-  console.log('clicked toggle');
-  
+function toggleSubMenu(e) {
+  const projectsButtonClicked = projectsMenuButton.contains(e.target);
+  const subMenuClicked = projectsSubMenu.contains(e.target);
+  if (projectsButtonClicked) {
+    projectsSubMenu.classList.toggle('show-sub-menu');
+  }
+  else if (!subMenuClicked) {
+    projectsSubMenu.classList.remove('show-sub-menu');
+  }
+  if (subMenuClicked && e.target.tagName === 'A') {
+    projectsSubMenu.classList.remove('show-sub-menu');
+  }
 }
