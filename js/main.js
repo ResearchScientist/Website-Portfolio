cc6c40be6c0a78f51e2fc64a8cf7ea21ad52be13
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2026-03-10 19:20:15 -0700

    [ui] main nav : add submenu

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ window.onfocus = function() {
 window.onblur = function() {
   document.title = "RS Portfolio : Come Back";
 }
+
+const projectsMenuButton = document.getElementById('projects-menu-button');
+const projectsSubMenu = document.getElementById('projects-sub-menu');
+
+projectsMenuButton.addEventListener('click',toggleSubMenu);
+
+function toggleSubMenu() {
+  projectsSubMenu.classList.toggle('show-sub-menu');
+  console.log('clicked toggle');
+  
+}
